b15709393823877e0629f8444266833ea10e5488
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-12-31 06:43:38 -0600

    working through Chapter 3 exercises; finishes 3.7.2 number 2

--- src/exercises/controlflow/Print5LetterWords.java
+++ src/exercises/controlflow/Print5LetterWords.java
@@ -6,14 +6,25 @@ import java.util.Scanner;
 public class Print5LetterWords {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter a word for the list: ");
-        String word = input.nextLine();
-
         ArrayList<String> list = new ArrayList<>();
+        String word;
 
-        if (word != "") {
-            list.add(word);
+        System.out.println("Enter a word for the list, or press ENTER to finish adding words: ");
+        do {
+
+            word = input.nextLine();
+
+            if (word != "") {
+                list.add(word);
+            }
+        } while (word != "");
+
+        System.out.println("Your list contains the following five-letter words: ");
+        for (String item : list) {
+
+            if (item.length() == 5) {
+                System.out.println(item);
+            }
         }
     }
 }
