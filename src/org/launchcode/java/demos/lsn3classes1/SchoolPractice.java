1e7d1adec59be215bc29c5d126460fa5d6c56376
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-01-08 15:54:16 -0600

    finishes class 3 (on classes, part 1) exercises 1, 2.

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,9 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student brian = new Student("Brian", 127, 3, 4.0);
+        System.out.println(brian.getStudent());
     }
+
+
 }
