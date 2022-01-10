282d4a51623a1c205a1e71fde26bfc553fd3a73c
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-01-10 12:26:21 -0600

    finishes exercises for chapter 4, Classes in Java part 1.

--- src/org/launchcode/java/demos/lsn3classes1/Teacher.java
+++ src/org/launchcode/java/demos/lsn3classes1/Teacher.java
@@ -18,16 +18,35 @@ public class Teacher {
         return firstName;
     }
 
+    public void setFirstName(String aFirstName) {
+        firstName = aFirstName;
+    }
+
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String aLastName) {
+        lastName = aLastName;
+    }
+
+    public void setName(String aLastName, String aFirstName) {
+        lastName = aLastName;
+        firstName = aFirstName;
+    }
     public String getSubject() {
         return subject;
+    }
+
+    public void setSubject(String aSubject) {
+        subject = aSubject;
     }
 
     public int getYearsTeaching() {
         return yearsTeaching;
     }
 
+    public void setYearsTeaching(int someYearsTeaching) {
+        yearsTeaching = someYearsTeaching;
+    }
 }
